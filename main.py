d89decbedadabfbef2fd63235779e5e74280405e
AlexTiptev <alex.tiptev@yandex.ru>
2026-01-20 21:36:30 +0300

    working start message

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ token = os.getenv("TOKEN")
 bot = telebot.TeleBot(token)
 
 print(bot)
-# @bot.message_handler(commands = ['start'])
-# def url(message):
-#     # markup = types.InlineKeyboardMarkup()
-#     # btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
-#     # markup.add(btn1)
-#     bot.send_message(message.from_user.id, "Прив")
+@bot.message_handler(commands = ['start'])
+def url(message):
+    # markup = types.InlineKeyboardMarkup()
+    # btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
+    # markup.add(btn1)
+    bot.send_message(message.from_user.id, "Прив")
 
 bot.polling(none_stop=True, interval=0)
